c3fadb78fec2dec87c586e65ddaf929fef49044b
AlexanderVG8 <alexander.v.g.8@gmail.com>
2017-06-01 14:36:47 +0300

    docs

---
+++ fillspreadsheet.py
@@ -0,0 +1,94 @@
+import requests
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+GET_TOKEN_URL = "https://target-sandbox.my.com/api/v2/oauth2/token.json"
+GET_DATA_URL = "https://target-sandbox.my.com/api/v1/campaigns.json?fields=id,status,stats_full"
+CRED_FILE="API Project-39ab4ed270d4.json"
+WRITER_EMAIL='selikhovalexey@gmail.com'
+SPREADSHEET_NAME="spreadsheet_name"
+
+
+def print_header(_sheet, _attempt_num):
+	_sheet.update_cell(1,1,"CampaingId")
+	_sheet.update_cell(1,2,"Date")
+	_sheet.update_cell(1,3,"Amount 1")
+	_sheet.update_cell(1,4,"Amount 2")
+	_sheet.update_cell(1,5,"Clicks 1")
+	_sheet.update_cell(1,6,"Clicks 2")
+	_sheet.update_cell(1,7,"Shows 1")
+	_sheet.update_cell(1,8,"Shows 2")
+	_sheet.update_cell(1,9,_attempt_num)
+
+
+def print_attempt(_sheet, _json, _attempt_num):
+	str_num = 2
+	for arr1 in _json:
+		for arr2 in arr1["stats_full"]:
+			_sheet.update_cell(str_num, 1, arr1["id"])
+			_sheet.update_cell(str_num, 2, arr2["date"])
+			_sheet.update_cell(str_num, 3 if _attempt_num == 1 else 4, arr2["amount"])
+			_sheet.update_cell(str_num, 5 if _attempt_num == 1 else 6, arr2["clicks"])
+			_sheet.update_cell(str_num, 7 if _attempt_num == 1 else 8, arr2["shows"])
+			str_num += 1
+
+
+formdata = {
+	"grant_type":"refresh_token",
+	"refresh_token":"DrNhXUd1aEnfMJYksISAHNKRXmYAARw2M0ULwY1aNSZrR7xNvlVnQHjkUZuc9R5kiQCT3OONhLnGPYIeL7HXecBo6r1zbSE9wRdZEiDfx9Xyd60tcigkriaAxGt4TkdGbB6BUBjLGFof5yQFQrHQaejMLGNkgwg0PIITkkLl58Lf1AiEu2PYmNhWkYqpUHhaPZ82t5Jlxv",
+	"client_secret":"zXHasS74dvjqdZG0rQjvTBsdHN2VkVnlkKpCwDgnMGh7p0y67mARKfsTZeXk8HVzYPWe2lcefb1P8VqzQHHCAPlISBM4S6pZKusOF82iZwgQ67AUvNxgTz2vkJIUzLWjLditYv5Os4uZQOcANBOhCCnAqO4JOVku4QwGhH5AeMb2sdkqPRXJRUddUX7GsQti9vW9nM1P5KjL5x8OFFox6eO6",
+	"client_id":"IXaxGDNypkGRYdBm",
+	"client_secret":"zXHasS74dvjqdZG0rQjvTBsdHN2VkVnlkKpCwDgnMGh7p0y67mARKfsTZeXk8HVzYPWe2lcefb1P8VqzQHHCAPlISBM4S6pZKusOF82iZwgQ67AUvNxgTz2vkJIUzLWjLditYv5Os4uZQOcANBOhCCnAqO4JOVku4QwGhH5AeMb2sdkqPRXJRUddUX7GsQti9vW9nM1P5KjL5x8OFFox6eO6"
+}
+
+try:
+	r = requests.post(GET_TOKEN_URL, formdata)
+except requests.exceptions.RequestException as e:
+	print(e)
+	sys.exit(1)
+
+if r.status_code != 200:
+	print("Token request is invalid or malformed")
+	sys.exit(1)
+
+access_token = r.json()["access_token"];
+
+try:
+	r = requests.get(GET_DATA_URL, headers={"Authorization":"Bearer {}".format(access_token)})
+except requests.exceptions.RequestException as e:
+	print(e)
+	sys.exit(1)
+
+if r.status_code != 200:
+	print("Data request is invalid")
+	sys.exit(1)
+
+scope=['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(CRED_FILE, scope)
+
+try:
+	client = gspread.authorize(creds)
+except:
+	print(sys.exc_info()[0])
+	sys.exit(1)
+
+try:
+	spreadsheet = client.open(SPREADSHEET_NAME)
+	sheet = spreadsheet.sheet1
+except gspread.SpreadsheetNotFound:
+	print("Spreadsheet not found. Create...")
+	spreadsheet = client.create(SPREADSHEET_NAME)
+	spreadsheet.share(WRITER_EMAIL, perm_type='user', role='writer')
+	sheet = spreadsheet.sheet1
+
+if sheet.cell(1,9).value=='2':
+	print("Clear sheet...")
+	attempt_num = 1
+	sheet.clear()
+else:
+	attempt_num = 2
+
+print_header(sheet, attempt_num)
+print_attempt(sheet, r.json(), attempt_num)
+
+print("Done.")
